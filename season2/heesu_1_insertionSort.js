917102eca42184e89260cd236c770120e679c43a
helloheesu <heesu@nhnnext.org>
2015-10-15 12:16:22 +0900

    insertion sort

---
+++ season2/heesu_1_insertionSort.js
@@ -0,0 +1,45 @@
+
+
+function insertionSort (array) {
+	if (array.length <= 1) {
+		return array;
+	}
+	for (var i = 1; i < array.length; i++) {
+		var currentElement = array[i];
+		for (var prePos = i-1; prePos >= 0 && array[prePos] > currentElement; prePos--) {
+			array[prePos+1] = array[prePos];
+		}
+		array[prePos+1] = currentElement;
+	}
+	return array;
+}
+
+var testArray = [5,2,4,6,1,3];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+
+var testArray = [1,3,6,2];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+var testArray = [1,2,3,6];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+var testArray = [2,3,6,1];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+var testArray = [6,3,2,1];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+
+var testArray = [2];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+var testArray = [];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+var testArray = [0];
+console.log(testArray);
+console.log('result',insertionSort(testArray));
+var testArray = [-1,0,4];
+console.log(testArray);
+console.log('result',insertionSort(testArray));

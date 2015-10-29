dc2a11f084752c24af4c854c1732d88d0a554604
helloheesu <heesu@nhnnext.org>
2015-10-29 16:43:32 +0900

    cleanse test codes

--- season2/heesu/1_insertionSort.js
+++ season2/heesu/1_insertionSort.js
@@ -1,14 +1,3 @@
-
-function isSorted (array) {
-	for (var i = 0; i< array.length - 1; i++) {
-		if (array[i] > array[i+1]) {
-			return false;
-		}
-	}
-	return true;
-}
-
-
 function insertionSort (array) {
 	if (array.length <= 1) {
 		return array;
@@ -22,46 +11,3 @@ function insertionSort (array) {
 	}
 	return array;
 }
-
-var testArray = [5,2,4,6,1,3];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-
-var testArray = [1,3,6,2];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-var testArray = [1,2,3,6];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-var testArray = [2,3,6,1];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-var testArray = [6,3,2,1];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-
-var testArray = [2];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-var testArray = [];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-var testArray = [0];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-var testArray = [-1,0,4];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-
-var testArray = [2,2,2];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-var testArray = [1,0,0,0,-2];
-console.log(testArray);
-console.log('result',insertionSort(testArray));
-
-/* occurs error. should deal with unexpected types.
-var testArray = [2, undefined, -1, 4];
-console.log(testArray);
-console.log('result',isSorted(testArray));
-*/
